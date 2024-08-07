71dcc1a5862bad6c4968dbd45a8b73a1ec8ac892
lyndalebdjiri <94696703+lyndalebdjiri@users.noreply.github.com>
2024-08-07 16:30:27 +0100

    Update lambda.py

--- Project-Developing-ML-Workflow/lambda.py
+++ Project-Developing-ML-Workflow/lambda.py
@@ -2,33 +2,55 @@ import json
 import boto3
 import base64
 import sagemaker
+import base64
 from sagemaker.serializers import IdentitySerializer
 
 
-def lambda_handler_serialize(event, context):
-    s3 = boto3.client('s3')
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+
+    # Get the s3 address from the Step Function event input
     key = event['s3_key']
     bucket = event['s3_bucket']
+
+    # Download the data from s3 to /tmp/image.png
     s3.download_file(bucket, key, '/tmp/image.png')
+
+    # We read the data from a file
     with open("/tmp/image.png", "rb") as f:
         image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
     return {
         'statusCode': 200,
         'body': {
-            "image_data": image_data,
+            "image_data": image_data.decode('utf-8'),
             "s3_bucket": bucket,
             "s3_key": key,
             "inferences": []
         }
     }
 
+# Fill this in with the name of your deployed model
+ENDPOINT = endpoint
 
-def lambda_handler_classify(event, context):
-    ENDPOINT = "your-endpoint-name"
+def lambda_handler(event, context):
+
+    # Decode the image data
     image = base64.b64decode(event['image_data'])
+
+    # Instantiate a Predictor
     predictor = sagemaker.predictor.Predictor(endpoint_name=ENDPOINT)
+
+    # For this model the IdentitySerializer needs to be "image/png"
     predictor.serializer = IdentitySerializer("image/png")
+
+    # Make a prediction:
     inferences = predictor.predict(image)
+
+    # We return the data back to the Step Function    
     event["inferences"] = inferences.decode('utf-8')
     return {
         'statusCode': 200,
@@ -36,15 +58,25 @@ def lambda_handler_classify(event, context):
     }
 
 
-def lambda_handler_filter(event, context):
-    THRESHOLD = .93
+THRESHOLD = .93
+
+def lambda_handler(event, context):
+
+    # Grab the inferences from the event
     inferences = json.loads(event['inferences'])
+
+    # Check if any values in our inferences are above THRESHOLD
     meets_threshold = any(i > THRESHOLD for i in inferences)
+
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
     if meets_threshold:
         pass
     else:
         raise Exception("THRESHOLD_CONFIDENCE_NOT_MET")
+
     return {
         'statusCode': 200,
         'body': json.dumps(event)
     }
+
